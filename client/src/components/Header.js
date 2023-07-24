36d4fa5a012be8a0e7476e276ecaed610ca93be8
daief48 <daief20103048@gmail.com>
2023-07-25 00:27:46 +0600

    add redux

---
+++ client/src/components/Header.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import {Box, AppBar, Toolbar, Button, Typography, Tabs, Tab} from "@mui/material";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+const Header = () => {
+    // global state
+    const isLogin = useSelector(sate => sate.isLogin)
+    console.log(isLogin);
+    // state
+    const [value, setValue] = useState();
+  return (
+    <AppBar position='sticky'>
+      <Toolbar>
+        <Typography variant='h4'>My Blog App</Typography>
+        {isLogin && (
+            <Box display={'flex'} marginLeft="auto" marginRight={'auto'}>
+        <Tabs textColor='inherit' value={value}>
+            <Tab label="Blogs" LinkComponent={Link} to="/blogs"/>
+            <Tab label="My Blogs" LinkComponent={Link} to='/my-blogs'/>
+        </Tabs>
+        </Box>
+        )}
+        <Box display={"flex"} marginLeft="auto">
+            {!isLogin && (<>
+                <Button sx={{margin:1, color:"white"}} LinkComponent={Link} to="/login">Login</Button>
+            <Button sx={{margin:1, color:"white"}} LinkComponent={Link} to="/register">Register</Button>
+            </>)}
+            {isLogin && (<Button sx={{margin:1, color:"white"}}>Logout</Button>)}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
