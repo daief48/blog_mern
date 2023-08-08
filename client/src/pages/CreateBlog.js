3d7c83ef99a9ad0fa997f65e01170d41a107793d
daief48 <105385149+daief48@users.noreply.github.com>
2023-08-09 01:50:00 +0600

    Update CreateBlog.js

--- client/src/pages/CreateBlog.js
+++ client/src/pages/CreateBlog.js
@@ -22,7 +22,7 @@ const CreateBlog = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("http://localhost:8080/api/v1/blog/create-blog", {
+      const { data } = await axios.post("https://blog-mern-backend-s00i.onrender.com/api/v1/blog/create-blog", {
         title: inputs.title,
         description: inputs.description,
         image: inputs.image,
